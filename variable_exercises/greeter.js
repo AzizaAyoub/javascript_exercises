69f142d72ef4dbd6ab6846a0339a08537f26ca96
Aziza Ayoub <meaziza0@gmail.com>
2023-09-27 11:01:05 +0430

    done the greeting exercise

---
+++ variable_exercises/greeter.js
@@ -0,0 +1,7 @@
+/*
+Write a program named greeter.js that greets 'Victor' three times. Your program should use a variable and not hard code the string value 'Victor' in each greeting. Here's an example run of the program:
+*/
+let name = 'victor';
+console.log(`Good Morning, ${name}.`);
+console.log(`Good Afternoon, ${name}.`);
+console.log(`Good Evning, ${name}`);

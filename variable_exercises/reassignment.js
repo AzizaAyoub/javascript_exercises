6bdf8a4c63b04aeabcf58f43e1fc9a1d12e7de23
Aziza Ayoub <meaziza0@gmail.com>
2023-09-28 10:07:38 +0430

    add the fourth exercise

---
+++ variable_exercises/reassignment.js
@@ -0,0 +1,13 @@
+// What happens when you run the following code? Why?
+
+const NAME = 'Ahmad';
+console.log(`Good Morning, ${NAME}`);
+console.log(`Good Afternoon, ${NAME}`);
+console.log(`Good Evening, ${NAME}`);
+
+NAME = 'Rahman';
+
+console.log(`Good Morning, ${NAME}`);
+console.log(`Good Afternoon, ${NAME}`);
+console.log(`Good Evening, ${NAME}`);
+

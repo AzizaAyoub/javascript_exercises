f213257fddc161e3d672c728744aa634dc70c619
Aziza Ayoub <meaziza0@gmail.com>
2023-09-28 09:56:00 +0430

    add the second exercise

---
+++ variable_exercises/age.js
@@ -0,0 +1,9 @@
+/*
+Write a program named age.js that includes someone's age and then calculates and reports the future age in 10, 20, 30 and 40 years. Below is the output for someone 20 years old.
+*/
+let age = 20;
+console.log(`You are ${age} years old`);
+console.log(`In 10 years, you will be ${10 + age} years old.`);
+console.log(`In 20 years, you will be ${20 + age} years old.`);
+console.log(`In 30 years, you will be ${30 + age} years old.`);
+console.log(`In 40 years, you will be ${40 + age} years old.`);

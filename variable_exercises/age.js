d21db6f2138a1b0f9782debfccfc5c9618fa441f
Aziza Ayoub <meaziza0@gmail.com>
2023-09-30 12:09:07 +0430

    add some changes

--- variable_exercises/age.js
+++ variable_exercises/age.js
@@ -1,7 +1,9 @@
 /*
 Write a program named age.js that includes someone's age and then calculates and reports the future age in 10, 20, 30 and 40 years. Below is the output for someone 20 years old.
 */
-let age = 20;
+
+let rlSync = require('readline-sync');
+let age = Number(rlSync.question('Enter your age '));
 console.log(`You are ${age} years old`);
 console.log(`In 10 years, you will be ${10 + age} years old.`);
 console.log(`In 20 years, you will be ${20 + age} years old.`);

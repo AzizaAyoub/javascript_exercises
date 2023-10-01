27a2eabc47df5aa6cdf43ba5cbfa7b619e39cbb3
Aziza Ayoub <meaziza0@gmail.com>
2023-10-01 10:25:02 +0430

    teast the prompt function

---
+++ input_output_exercises/personalized_greeting_browser.js
@@ -0,0 +1,6 @@
+/*
+Since our HTML expects a personalized_greeting_browser.js file, go ahead and create one with the code shown below. Place it in the same directory as the HTML file:
+*/
+
+let name = prompt('What is your name? ');
+console.log(`Good Morning, ${name}!`);

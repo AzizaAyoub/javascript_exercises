ca5d3ae62ad00295d50e802918a8981582e07ef5
Aziza Ayoub <meaziza0@gmail.com>
2023-10-01 10:45:43 +0430

    use the console.log many time

---
+++ function_exercises/say2.js
@@ -0,0 +1,18 @@
+// log the below words to the console
+
+console.log('Hello');
+console.log('Hi!');
+console.log('How do you do?');
+console.log('quite all right');
+
+
+// let's create a function
+
+function say2(text) {
+    console.log(text)
+}
+
+say2('Hello');
+say2('Hi!');
+say2('How do you do?');
+say2('quite all right');

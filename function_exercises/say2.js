04fa2c1e05a7bae2c048b72d385c4149ec068ec7
Aziza Ayoub <meaziza0@gmail.com>
2023-10-02 10:38:03 +0430

    add some changes

--- function_exercises/say2.js
+++ function_exercises/say2.js
@@ -8,8 +8,12 @@ console.log('quite all right');
 
 // let's create a function
 
+/*
+As mentioned earlier, one benefit that functions give us is the ability to make changes in one location. Suppose we want to add a ==> to the beginning of every line that say outputs.
+*/
+
 function say2(text) {
-    console.log(text)
+    console.log('==> ' + text);
 }
 
 say2('Hello');

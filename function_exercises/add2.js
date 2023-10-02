533d826eedbf5c754a3f81e7c1ad15dc9da59d88
Aziza Ayoub <meaziza0@gmail.com>
2023-10-02 10:42:24 +0430

    uses the return statement

---
+++ function_exercises/add2.js
@@ -0,0 +1,11 @@
+// Let's create an add function that returns the sum of two numbers:
+
+function add(a, b) {
+    return a + b;
+}
+let num = add(2, 3);
+console.log(num);
+
+/*
+When JavaScript encounters the return statement, it evaluates the expression, terminates the function, and returns the expression's value to the location where we called it
+*/

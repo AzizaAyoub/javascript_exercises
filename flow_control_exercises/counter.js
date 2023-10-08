119c4d39da4cb5b767afdfe3300b9f371ad2c3e3
Aziza Ayoub <meaziza0@gmail.com>
2023-10-08 17:28:20 +0430

    add the exercise

---
+++ flow_control_exercises/counter.js
@@ -0,0 +1,13 @@
+
+let counter1 = 0;
+while (counter1 < 10) {
+    console.log(counter1);
+counter1 = counter1 + 1;
+}
+
+
+let counter3 = 1;
+while (counter3 <= 1000) {
+  console.log(counter3);
+  counter3 = counter3 + 1;
+}

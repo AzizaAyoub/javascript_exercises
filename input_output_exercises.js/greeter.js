119dd4cbb0e2af13dbeec1b0353034ef2252918c
Aziza Ayoub <meaziza0@gmail.com>
2023-10-08 17:14:26 +0430

    "add some changes"

--- input_output_exercises.js/greeter.js
+++
@@ -1,8 +0,0 @@
-/*
-Write a dynamic greeter program named greeter.js. The program should ask for your name, then output Hello, {name}! where {name} is the name you entered:
-*/
-
-let rlSync = require('readline-sync');
-let firstName = rlSync.question('What is your first name? ');
-let lastName = rlSync.question('What is your last name? ');
-console.log(`Hello, ${firstName} ${lastName}!`);

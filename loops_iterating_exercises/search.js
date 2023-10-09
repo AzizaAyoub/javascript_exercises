3ccdf483b6a10db33f4a4f921a53d7a033823214
Aziza Ayoub <meaziza0@gmail.com>
2023-10-09 14:40:20 +0430

    add the exercies

---
+++ loops_iterating_exercises/search.js
@@ -0,0 +1,12 @@
+// Let's explore this idea with some code. Create a file named search.js with the following code and run it in your browser or Node:
+
+let array = [3, 1, 5, 9, 2, 6, 4, 7];
+let indexOfFive = -1;
+
+for (let i = 0; i < array.length; i += 1) {
+  if (array[i] === 5) {
+    indexOfFive = i;
+  }
+}
+
+console.log(indexOfFive);

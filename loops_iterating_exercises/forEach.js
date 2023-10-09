0327b3d7727e240d6f05ce9b442cd0e907a6d850
Aziza Ayoub <meaziza0@gmail.com>
2023-10-09 14:48:28 +0430

    add the exercises

---
+++ loops_iterating_exercises/forEach.js
@@ -0,0 +1,9 @@
+// Suppose you have an array of names that you want to display on the console, one per line
+// One way is to use the built-in forEach method for arrays:
+
+
+let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor'];
+
+names.forEach(function(name) {
+  console.log(name);
+});

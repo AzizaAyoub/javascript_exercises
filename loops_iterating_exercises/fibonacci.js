db7349a74b11c21b32f6aa78a3be45fa97a0893d
Aziza Ayoub <meaziza0@gmail.com>
2023-10-12 11:29:57 +0430

    add some changes to the function

--- loops_iterating_exercises/fibonacci.js
+++ loops_iterating_exercises/fibonacci.js
@@ -10,3 +10,20 @@ function fibonacci(number) {
 /*
 Each time the fibonacci function runs, it recursively invokes itself twice, once with a number 1 less than the current number, and once with a number 2 less than the current number.
 */
+
+function factorial(number) {
+  if (number === 1) {
+    return 1;
+  }
+
+  return number * factorial(number - 1);
+}
+
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320

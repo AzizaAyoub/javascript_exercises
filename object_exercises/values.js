39f598873515b670cafa553e4a034f463b04cb13
Aziza Ayoub <meaziza0@gmail.com>
2023-10-21 17:21:20 +0430

    copy object

--- object_exercises/values.js
+++ object_exercises/values.js
@@ -7,7 +7,7 @@
 false // primitive value
 function bar() { return "bar"; } // (fuctions are object)
 undefined // primitive value
-{ a: 1, b: 2 } // object
+{ a: 2 } // object
 foo // neither primitive nor object
 // foo is an identifier. Identifiers are used to name things that have values, such as variables and functions, but they are not values by themselves. Thus, they are neither primitive values nor objects.
 

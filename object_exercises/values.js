5d56bce0b4bf96719b85a663a42b500c3e27c514
Aziza Ayoub <meaziza0@gmail.com>
2023-10-21 17:08:24 +0430

    data types

---
+++ object_exercises/values.js
@@ -0,0 +1,13 @@
+// Which of the following values are primitive values? Which are objects? Which are neither?
+
+
+"foo" // primitive value
+3.1415 // primitive value
+[ 'a', 'b', 'c' ] // (arrays are object)
+false // primitive value
+function bar() { return "bar"; } // (fuctions are object)
+undefined // primitive value
+{ a: 1, b: 2 } // object
+foo // neither primitive nor object
+// foo is an identifier. Identifiers are used to name things that have values, such as variables and functions, but they are not values by themselves. Thus, they are neither primitive values nor objects.
+

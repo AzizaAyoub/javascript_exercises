5c505a4ed3c565418ee054bb8050445c174669ed
Aziza Ayoub <meaziza0@gmail.com>
2023-10-24 21:41:09 +0430

    identifiers the primitive values

---
+++ object_exercises/primitive_value.js
@@ -0,0 +1,14 @@
+// How many primitive values are there in the following expression? Identify them. How many objects are there in the expression? Identify those objects.
+
+[1, 2, ["a", ["b", false]], null, {}]
+
+// There are 6 primitive values and 4 objects:
+
+
+// Primitive Values	      //Objects
+1	                 [1, 2, ["a", ["b", false]], null, {}]
+2	                 ["a", ["b", false]]
+"a"	                 ["b", false]
+"b"	                 {}
+false	
+null	
